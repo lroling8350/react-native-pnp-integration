e41f4d19f1d3530054318e17d4e63ffad0e2824b
Roling, Luke <LRoling@cvent.com>
2019-11-08 16:31:45 -0500

    initial create react native apps

---
+++ packages/example-app-a/ios/Pods/Target Support Files/Pods-ExampleAppATests/Pods-ExampleAppATests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExampleAppATests : NSObject
+@end
+@implementation PodsDummy_Pods_ExampleAppATests
+@end
---
+++ packages/example-app-b/ios/Pods/Target Support Files/Pods-ExampleAppB/Pods-ExampleAppB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExampleAppB : NSObject
+@end
+@implementation PodsDummy_Pods_ExampleAppB
+@end
